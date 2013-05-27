550a3868e5dd8eb35c2a084110de840bf33fd76e
Chase Tingley <chase@spartansoftwareinc.com>
2013-05-26 21:07:11 -0700

    Add basic logging of task types and the types of errors found

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,12 +7,16 @@ import java.io.InputStream;
 
 import com.globalsight.tip.InMemoryBackingStore;
 import com.globalsight.tip.TIPP;
+import com.globalsight.tip.TIPPError;
+import com.globalsight.tip.TIPPErrorSeverity;
 import com.globalsight.tip.TIPPFactory;
 import com.globalsight.tip.TIPPLoadStatus;
 
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.*;
+
+import play.Logger;
 
 import views.html.*;
 
@@ -30,11 +34,13 @@ public class Application extends Controller {
     		return redirect(routes.Application.index());
     	}
     	String fileName = file.getFilename();
+    	Logger.info("Validating " + fileName + " from " + request().remoteAddress());
     	try {
 	    	TIPPLoadStatus loadStatus = new TIPPLoadStatus();
 	    	InputStream is = new BufferedInputStream(new FileInputStream(file.getFile()));
 	    	TIPP tipp = TIPPFactory.openFromStream(is, new InMemoryBackingStore(), loadStatus);
 	    	is.close();
+	    	logIssues(fileName, tipp, loadStatus);
 	    	switch (loadStatus.getSeverity()) {
 	    	case NONE:
 	    		return ok(valid.render(fileName, tipp, loadStatus));
@@ -50,6 +56,17 @@ public class Application extends Controller {
     	catch (IOException e) {
     		flash("error", "Error reading file: " + e.getMessage());
     		return redirect(routes.Application.index());
+    	}
+    }
+    
+    private static void logIssues(String fileName, TIPP tipp, TIPPLoadStatus status) {
+    	if (status.getSeverity() == TIPPErrorSeverity.NONE || 
+    		status.getSeverity() == TIPPErrorSeverity.WARN) {
+    		Logger.info(fileName + ": task type " + tipp.getTaskType());
+    	}
+    	for (TIPPError error : status.getAllErrors()) {
+    		TIPPError.Type t = error.getErrorType();
+    		Logger.info(fileName + " " + t.getSeverity() + ": " + error);
     	}
     }
 }
